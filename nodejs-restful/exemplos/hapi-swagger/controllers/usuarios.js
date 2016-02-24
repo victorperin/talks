c843c7e82c79a15afa342438e9ef5f3c8c6122cb
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-24 20:13:08 -0300

    Perfil de usuários, retornando erros com Boom.

--- nodejs-restful/exemplos/hapi-swagger/controllers/usuarios.js
+++ nodejs-restful/exemplos/hapi-swagger/controllers/usuarios.js
@@ -1,7 +1,7 @@
 'use strict'
 
-// Documentação do lodash: https://lodash.com/docs
-var _ = require('lodash')
+var _ = require('lodash') // Documentação do lodash: https://lodash.com/docs
+var Boom = require('boom') // Erros HTTP amigáveis
 
 // Dados fictícios para estes exemplos
 var dados = require('../dados/usuarios')
@@ -12,6 +12,21 @@ module.exports = [
     path: '/usuarios',
     handler: function (req, reply) {
       return reply(dados)
+    }
+  },
+  {
+    method: 'GET',
+    path: '/usuarios/{username}',
+    handler: function (req, reply) {
+      var usuario = _.find(dados, function (i) {
+        return i.username == req.params.username
+      })
+
+      if (usuario) {
+        reply(usuario)
+      } else {
+        reply(Boom.notFound('Usuário não cadastrado!'))
+      }
     }
   }
 ]
