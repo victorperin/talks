ad4e31e3c2456d03c97f39fa2b985fbad0fc9368
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-24 20:21:27 -0300

    Criação de usuários com retorno 201.

--- nodejs-restful/exemplos/hapi-swagger/controllers/usuarios.js
+++ nodejs-restful/exemplos/hapi-swagger/controllers/usuarios.js
@@ -1,7 +1,7 @@
 'use strict'
 
-var _ = require('lodash') // Documentação do lodash: https://lodash.com/docs
-var Boom = require('boom') // Erros HTTP amigáveis
+const _ = require('lodash') // Documentação do lodash: https://lodash.com/docs
+const Boom = require('boom') // Erros HTTP amigáveis
 
 // Dados fictícios para estes exemplos
 var dados = require('../dados/usuarios')
@@ -18,14 +18,42 @@ module.exports = [
     method: 'GET',
     path: '/usuarios/{username}',
     handler: function (req, reply) {
-      var usuario = _.find(dados, function (i) {
+      let usuario = _.find(dados, function (i) {
         return i.username == req.params.username
       })
 
+      // Usuário existe, retorna seu perfil
       if (usuario) {
         reply(usuario)
       } else {
+        // Usuário não existe, retorna erro 404 não encontrado
         reply(Boom.notFound('Usuário não cadastrado!'))
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/usuarios',
+    handler: function (req, reply) {
+      let idx = _.findIndex(dados, function (i) {
+        return i.username == req.payload.username
+      })
+
+      let detalhes = {
+        username: req.payload.username,
+        full_name: req.payload.full_name,
+        twitter: req.payload.twitter
+      }
+
+      console.log(detalhes)
+
+      // Usuário não existe, vamos inseri-lo
+      if (idx === -1) {
+        dados.push(detalhes)
+        reply(detalhes).created(`/usuarios/${detalhes.username}`)
+      } else {
+        // Usuário já existe, retorna erro 405 não permitido
+        reply(Boom.methodNotAllowed('Nome de usuário já cadastrado!'))
       }
     }
   }
