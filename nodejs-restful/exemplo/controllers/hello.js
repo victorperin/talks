c2cb62332dac7c19f52367b4490c507cb08155d5
Filipe Oliveira <contato@fmoliveira.com.br>
2016-02-23 22:16:36 -0300

    Documentando controller de saudações.

--- nodejs-restful/exemplo/controllers/hello.js
+++ nodejs-restful/exemplo/controllers/hello.js
@@ -1,5 +1,14 @@
 var ctrl = {}
 
+/**
+ * @api {get} /hello/:nome? Cumprimentar o usuário.
+ * @apiName Cumprimentar usuário.
+ * @apiGroup Cumprimentos.
+ *
+ * @apiParam {String} [nome] Nome do usuário a ser cumprimentado.
+ *
+ * @apiSuccess {String} . Mensagem de saudação ao usuário.
+ */
 ctrl.saudacao = function (req, res, next) {
   var nome = req.params.nome || 'mundo'
   res.send('Hello, ' + nome + '!')
